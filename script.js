da9231080f2798eb27ef31654459d1657c0bf335
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-10 18:47:21 +0200

    defined basic words and initial values

--- script.js
+++ script.js
@@ -5,3 +5,34 @@ const newGameContainer = document.getElementById("new-game-container");
 const newGameButton = document.getElementById("new-game-button");
 const canvas = document.getElementById("canvas");
 const resultText = document.getElementById("result-text");
+
+let options = {
+    fruits: [
+        "Apple",
+        "Blueberry",
+        "Mandarin",
+        "Pineapple",
+        "Pomegranate",
+        "Watermelon",
+    ],
+    animals: [
+        "Hedgehog",
+        "Rhinoceros",
+        "Squirrel",
+        "Panther",
+        "Walrus",
+        "Zebra"
+    ],
+    countries: [
+        "India",
+        "Hungary",
+        "Kyrgyzstan",
+        "Switzerland",
+        "Zimbabwe",
+        "Dominica",
+    ]
+};
+
+let winCount = 0;
+let count = 0;
+let chosenWord = "";
